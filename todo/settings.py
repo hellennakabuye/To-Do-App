b6998756a84a4798d54b629daab812c4efb67045
hellennakabuye <55150746+hellennakabuye@users.noreply.github.com>
2023-07-26 16:33:46 +0300

    Update settings.py

--- todo/settings.py
+++ todo/settings.py
@@ -24,11 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-o6b00m&zu8-%_#xr)4li0v*j47q1l5c0co05qn&9c!-is48)yt'
-SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
+SECRET_KEY = 'django-insecure-o6b00m&zu8-%_#xr)4li0v*j47q1l5c0co05qn&9c!-is48)yt'
+# SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
-DEBUG = os.getenv("DEBUG", "False") == "True"
+DEBUG = True
+# DEBUG = os.getenv("DEBUG", "False") == "True"
 ALLOWED_HOSTS = [".vercel.app"]
 # ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 
